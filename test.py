d9da9a4eeccf2ba7ba2d4d3392ac610febb308d6
Simon Jonsson <simon.jonsson1999@outlook.com>
2024-02-09 12:45:11 +0100

    cr

--- test.py
+++ test.py
@@ -1,10 +1,24 @@
 import torch
 from denoising_diffusion_pytorch import Unet, GaussianDiffusion, Dataset_own, Trainer
 
+
+if torch.cuda.is_available():
+    # Print the number of CUDA devices available
+    num_gpus = torch.cuda.device_count()
+    print(f"Number of CUDA devices available: {num_gpus}")
+    
+    # Iterate over available CUDA devices and print their names
+    for i in range(num_gpus):
+        gpu_name = torch.cuda.get_device_name(i)
+        print(f"GPU {i}: {gpu_name}")
+else:
+    print("CUDA is not available.")
 model = Unet(
-    dim = 64,
+    dim = 128,
     dim_mults = (1, 2, 4, 8),
-    flash_attn = True
+    flash_attn = True,
+    attn_dim_head = 32,
+    attn_heads = 4,
 )
 
 diffusion = GaussianDiffusion(
@@ -13,18 +27,16 @@ diffusion = GaussianDiffusion(
     timesteps = 1000    # number of steps
 )
 
-# training_images = torch.rand(8, 3, 128, 128) # images are normalized from 0 to 1
-# loss = diffusion(training_images)
+trainer = Trainer(diffusion_model=diffusion,
+                    folder = "data",
+                    gradient_accumulate_every=4,
+                    train_batch_size=16,
+                    train_lr=1e-5,
+                    train_num_steps=7_500,
+                    save_and_sample_every=1_000,
+                    calculate_fid=False)
 
-# loss.backward()
 
-# # after a lot of training
-
-# sampled_images = diffusion.sample(batch_size = 4)
-# sampled_images.shape # (4, 3, 128, 128)
-
-
-# dataset = Dataset_own(folder="data", image_size=256)
-trainer = Trainer(diffusion_model=diffusion, folder = "data")
 trainer.train()
-trainer.save("test")
+trainer.save("test_15_000")
+
